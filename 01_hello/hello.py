1e8784e049028661983a4cf7c0b19584e47e9e3e
Charles De Mount <thexrayone@icloud.com>
2021-01-23 20:58:01 -0500

    solution for exercise 1

---
+++ 01_hello/hello.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Author: Charles De Mount <thexrayone@icloud.com>
+Purpose: Say hello
+"""
+
+import argparse
+
+
+def get_args():
+    """Get the command-line arguments"""
+
+    parser = argparse.ArgumentParser(description="Say hello")
+    parser.add_argument(
+        "-n", "--name", metavar="name", default="World", help="Name to greet"
+    )
+    return parser.parse_args()
+
+
+def main():
+    """Execute and print to the console"""
+
+    args = get_args()
+    print(f"Hello, {args.name}!")
+
+
+if __name__ == "__main__":
+    main()
